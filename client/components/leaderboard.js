402a84f363682ab07b598882ce31774c924d714c
driver620 <drivera453@hotmail.com>
2019-07-02 11:06:31 -0400

    Chore: made leaderboard component. Added leaderboard client route

---
+++ client/components/leaderboard.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+
+export default class Leaderboard extends Component {
+  render() {
+    return <h1>Leaderboard</h1>
+  }
+}
