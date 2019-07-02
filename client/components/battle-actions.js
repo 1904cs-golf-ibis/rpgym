e0d8c73d6b5294190f9ea76c0d3669995f5c2d09
driver620 <drivera453@hotmail.com>
2019-07-02 16:45:17 -0400

    Chore: created battle-room and battle-actions files. Added BattleRoom component to NavBar and routes

---
+++ client/components/battle-actions.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const battleActions = props => {
+  return (
+    <div>
+      <button type="submit">Rock</button>
+      <button type="submit">Paper</button>
+    </div>
+  )
+}
---
+++ client/components/battle-room.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+// import {connect} from 'react-redux'
+import BattleActions from './battle-actions'
+
+export default class Leaderboard extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Battle Room</h1>
+        {/* <BattleActions /> */}
+      </div>
+    )
+  }
+}
