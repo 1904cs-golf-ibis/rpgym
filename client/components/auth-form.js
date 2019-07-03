2778ad0c5bcce70f96d06edd4de144552c03e42e
daphne178 <realta23dc@gmail.com>
2019-07-03 15:03:38 -0400

    fix: deleted the login form

--- client/components/auth-form.js
+++ client/components/auth-form.js
@@ -11,24 +11,6 @@ const AuthForm = props => {
 
   return (
     <div>
-      <form onSubmit={handleSubmit} name={name}>
-        <div>
-          <label htmlFor="email">
-            <small>Email</small>
-          </label>
-          <input name="email" type="text" />
-        </div>
-        <div>
-          <label htmlFor="password">
-            <small>Password</small>
-          </label>
-          <input name="password" type="password" />
-        </div>
-        <div>
-          <button type="submit">{displayName}</button>
-        </div>
-        {error && error.response && <div> {error.response.data} </div>}
-      </form>
       <a href="/auth/strava">{displayName} with Strava</a>
     </div>
   )
