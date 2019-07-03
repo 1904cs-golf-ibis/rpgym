4689e878f80d70268eed258746eb1eb207d87e15
driver620 <drivera453@hotmail.com>
2019-07-02 20:25:30 -0400

    Chore: added form to battle message component

--- client/components/battle-messages.js
+++ client/components/battle-messages.js
@@ -1,13 +1,41 @@
 import React, {Component} from 'react'
-// import {connect} from 'react-redux'
+import {connect} from 'react-redux'
 
 export default class BattleMessages extends Component {
+  constructor() {
+    super()
+    this.state = ''
+  }
+  handleChange = event => {
+    event.preventDefault()
+    console.log(event.name)
+  }
+  handleSumbit = event => {
+    event.preventDefault()
+    console.log(event.value)
+  }
   componentDidMount() {}
   render() {
     return (
       <div>
         <h1>Battle Messages</h1>
+        <form onSubmit={this.handleSumbit}>
+          <button type="submit" value="Rock" onChange={this.handleChange}>
+            Rock
+          </button>
+          <button type="submit" value="Paper" onChange={this.handleChange}>
+            Paper
+          </button>
+        </form>
       </div>
     )
   }
+}
+
+const mapStateToProps = state => ({
+  messages: state.battles
+})
+
+const mapDispatchToProps = dispatch => {
+  // fetchAllMessages: ()
 }
--- client/components/battle-room.js
+++ client/components/battle-room.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react'
 // import {connect} from 'react-redux'
 import BattleActions from './battle-actions'
+import BattleMessages from './battle-messages'
 
 export default class BattleRoom extends Component {
   render() {
     return (
       <div>
         <h1>Battle Room</h1>
+        <BattleMessages />
       </div>
     )
   }
