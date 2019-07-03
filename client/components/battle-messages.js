ce98248fe64878bb2db8a105062f5ac1641d4d78
driver620 <drivera453@hotmail.com>
2019-07-02 20:47:19 -0400

    Chore: Battle messages component now updates battle reducer state when clicking rock

--- client/components/battle-messages.js
+++ client/components/battle-messages.js
@@ -1,13 +1,46 @@
 import React, {Component} from 'react'
-// import {connect} from 'react-redux'
+import {connect} from 'react-redux'
+import {
+  getNewBattleMessageThunkCreator,
+  getBattleMessagesThunkCreator
+} from '../store/battle'
 
-export default class BattleMessages extends Component {
+class BattleMessages extends Component {
+  constructor() {
+    super()
+    this.state = ''
+  }
+
+  handleSumbit = event => {
+    event.preventDefault()
+    console.log(event.target.innerText)
+    this.props.fetchNewMessage(event.target.innerText)
+  }
   componentDidMount() {}
   render() {
     return (
       <div>
         <h1>Battle Messages</h1>
+        <form onSubmit={this.handleSumbit}>
+          <button type="submit" value="Rock" onChange={this.handleChange}>
+            Rock
+          </button>
+          {/* <button type="submit" value="Paper" onChange={this.handleChange}>
+            Paper
+          </button> */}
+        </form>
       </div>
     )
   }
 }
+
+const mapStateToProps = state => ({
+  messages: state.battles
+})
+
+const mapDispatchToProps = dispatch => ({
+  fetchAllMessages: messages => dispatch(getBattleMessagesThunkCreator()),
+  fetchNewMessage: message => dispatch(getNewBattleMessageThunkCreator(message))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(BattleMessages)
