c52f24fd4516bca2e8c22be76973b2e065981a6d
Arnald Wong <arny1031@gmail.com>
2019-07-07 16:24:10 -0400

    simple battle animation setup

--- client/components/arena.js
+++ client/components/arena.js
@@ -8,13 +8,19 @@ export default class Arena extends Component {
     return (
       <div>
         <div id="arena">
-          <img
+          {/* <img
             id="arenaImage"
             src="https://i.ytimg.com/vi/ucjk7FByCq8/maxresdefault.jpg"
             width="75%"
             align="center"
-          />
+          /> */}
           <div id="arenaAvatar">
+            <img
+              src="http://pixelartmaker.com/art/6bb9673b65fade0.png"
+              width="25%"
+            />
+          </div>
+          <div id="arenaOpponent">
             <img
               src="http://pixelartmaker.com/art/6bb9673b65fade0.png"
               width="25%"
